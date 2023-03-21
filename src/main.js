6b075d62dc65f743c4c5e02c0de48766b8a17fc0
Raju George <admin@goingcloudnative.com>
2023-03-21 16:47:50 +0530

    initial version completed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-import './style.css'
+const mainNav = document.getElementById('main-navigation');
+const navOpenBtn = document.getElementById('nav-open-btn');
+const navCloseBtn = document.getElementById('nav-close-btn')
+
+navOpenBtn.addEventListener('click', (e) => {
+    mainNav.dataset.menu = "open"
+})
+
+navCloseBtn.addEventListener('click', (e) => {
+    mainNav.dataset.menu = "close"
+})
+
+window.addEventListener('resize', (e) => {
+    mainNav.dataset.menu = "close"
+})
